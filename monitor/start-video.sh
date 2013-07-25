a027c1e7cff503b5480cfcc6c2ef4282df0efe3a
Hawkes <github@the-hawkes.de>
2013-07-25 16:40:39 +0200

    make it a little bit nicer

--- monitor/start-video.sh
+++ monitor/start-video.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-mjpg_streamer -i "libinput_uvc.so.0 -d /dev/video0 -f 24" -o "liboutput_http.so.0 -p 8080"
+RESOLUTION="640x480"
+FPS="24"
+PORT="8080"
+mjpg_streamer -i "libinput_uvc.so.0 -d /dev/video0 -r $RESOLUTION -f $FPS" -o "liboutput_http.so.0 -p $PORT"
